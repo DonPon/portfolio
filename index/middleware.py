868e3bf48acc5426a3ee91f23cc4786a336b636c
DonPon <franzeckermann@gmail.com>
2024-10-03 20:04:39 +0200

    added contact form

---
+++ index/middleware.py
@@ -0,0 +1,27 @@
+from datetime import timedelta
+from django.utils import timezone
+from .models import Visitor
+from django.utils.deprecation import MiddlewareMixin
+
+class LogVisitorMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        # Get the user's IP address
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+
+        # Get the user agent (browser and OS information)
+        user_agent = request.META.get('HTTP_USER_AGENT', '')
+
+        # Check if the IP was logged within the last hour (for example)
+        last_hour = timezone.now() - timedelta(hours=1)
+        existing_visitor = Visitor.objects.filter(ip_address=ip, visit_time__gte=last_hour).exists()
+
+        if not existing_visitor:
+            # Save the visitor information to the database
+            Visitor.objects.create(
+                ip_address=ip,
+                user_agent=user_agent
+            )
