2b792f59a91fa9d64e3c8725a7c84ec0c11e5127
DonPon <franzeckermann@gmail.com>
2024-10-03 18:40:50 +0200

    more cool changes

--- index/admin.py
+++ index/admin.py
@@ -1,6 +1,13 @@
 # index/admin.py
-
+from django.apps import apps
 from django.contrib import admin
-from .models import PortfolioItem
+from django.contrib.admin.sites import AlreadyRegistered
 
-admin.site.register(PortfolioItem)
+# Automatically register all models from all installed apps
+app_models = apps.get_models()
+
+for model in app_models:
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered:
+        pass  # Some models might already be registered, so we skip those
